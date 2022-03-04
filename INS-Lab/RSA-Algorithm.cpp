6e7d49410a720157bc0a10df1bf75827f99e0fc6
Kritik007 <kritikagarwal@ymail.com>
2022-03-04 18:31:50 +0530

    Final Commit

--- INS-Lab/RSA-Algorithm.cpp
+++ INS-Lab/RSA-Algorithm.cpp
@@ -1,62 +1,125 @@
 // Author: Kritik Agarwal <https://github.com/Kritik007>
 
-// Implementation of RSA Algorithm in C++
+// Implementation of RSA Algorithm in C++ to enCrypt and deCrypt plaintext
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-// Function to Find Greatest Common Divisor
-int GCD(int x, int y)
+void heading()
 {
-    int tmp;
-    while (1)
-    {
-        tmp = x % y;
-        if (tmp == 0)
-            return y;
-        x = y;
-        y = tmp;
-    }
+    system("cls");
+    cout << "-------------------------------------------------------------------------------------------" << endl;
+    cout << "|                                KRITIK AGARWAL 19BBTCS067                                |" << endl;
+    cout << "-------------------------------------------------------------------------------------------" << endl;
 }
 
+// Function to find Greatest Common Divisor
+long int GCD(long int a, long int b)
+{
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+    return GCD(b, a % b);
+}
+
+// Function to check whether the number is prime or not
+long int isPrime(long int a)
+{
+    int i;
+    for (i = 2; i < a; i++)
+    {
+        if ((a % i) == 0)
+            return 0;
+    }
+    return 1;
+}
+
+// Function to encrypt the plaintext message
+long int enCrypt(char ch, long int n, long int e)
+{
+    int i;
+    long int temp = ch;
+    for (i = 1; i < e; i++)
+        temp = (temp * ch) % n;
+    return temp;
+}
+
+// Function to decrypt the plaintext message
+char deCrypt(long int ch, long int n, long int d)
+{
+    int i;
+    long int temp = ch;
+    for (i = 1; i < d; i++)
+        ch = (temp * ch) % n;
+    return ch;
+}
+
+// Main Function
 int main()
 {
-    double p, q;
-    cout << ">>>  Enter Two Co-prime Numbers:  ";
-    cin >> p >> q;
-    double n = p * q;
-    double cnt;
-    double fy = (p - 1) * (q - 1);
+    heading();
+    long int i, len;
+    long int p, q, n, fy, e, d, cipher[50];
+    char text[50];
 
-    // Public Key
-    // enc stands for encrypt
-    double enc = 2;
-    while (enc < fy)
+    // Get input of plaintext here
+    cout << ">> Enter the text to be encrypted: ";
+    cin.getline(text, sizeof(text));
+    len = strlen(text);
+
+    // Generate Two Random Prime Numbers for encryption and decryption
+    do
     {
-        cnt = GCD(enc, fy);
-        if (cnt == 1)
-            break;
-        else
-            enc++;
-    }
+        p = rand() % 30;
+    } while (!isPrime(p));
 
-    // Private Key
-    // dcry stands for decrypt
-    double dcry;
-    double k = 2; // k can be any arbitrary value
+    do
+    {
+        q = rand() % 30;
+    } while (!isPrime(q));
 
-    // Choosing dcry such that it satisfies the condition dcry*enc = 1 + k * fy
-    dcry = (1 + (k * fy)) / enc;
-    double msg;
-    cout << ">>>  Enter a message:  ";
-    cin >> msg;
-    double c = pow(msg, enc);
-    double m = pow(c, dcry);
-    c = fmod(c, n);
-    m = fmod(m, n);
-    cout << "\nMessage data = " << msg;
-    cout << "\nEncrypted data = " << c;
-    cout << "\nOriginal Message sent = " << m << endl;
+    // Calculation of value of n and ɸ[fy]
+    n = p * q;
+    fy = (p - 1) * (q - 1);
+
+    // Generate Value of e (encryption) and d (decryption) such that it satifies the condition gcd(ɸ,e)=1
+    do
+    {
+        e = rand() % fy;
+    } while (GCD(fy, e) != 1);
+
+    do
+    {
+        d = rand() % fy;
+    } while (((d * e) % fy) != 1);
+
+    // Display the generated prime numbers
+    cout << "\n>> Two prime numbers (p and q) are: " << p << " and " << q << endl;
+
+    // Display the generated public and private keys
+    cout << ">> Public Key (n, e): (" << n << ", " << e << ")\n";
+    cout << ">> Private key (n, d): (" << n << ", " << d << ")\n";
+
+    // Encrypt the message through loop
+    for (i = 0; i < len; i++)
+        cipher[i] = enCrypt(text[i], n, e);
+
+    // Display the encrypted message
+    cout << ">> Encrypted message: ";
+    for (i = 0; i < len; i++)
+        cout << cipher[i];
+
+    // Decrypt the message through loop
+    for (i = 0; i < len; i++)
+        text[i] = deCrypt(cipher[i], n, d);
+
+    // Display the decrypted message
+    cout << "\n>> Decrypted message: ";
+    for (i = 0; i < len; i++)
+        cout << text[i];
+
+    cout << endl;
     return 0;
 }
